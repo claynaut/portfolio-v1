0c5d2aefd38f964b892a0b0ab50f8195c77c7700
claynaut <jpesc002@ucr.edu>
2021-02-05 12:18:10 -0800

    Update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,19 @@ module.exports = {
     author: `J.S. Pescasio`,
     twitterUsername: `@claynaut`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/images/favicon.ico`,
+      },
+    },
+  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,14 +10,6 @@ export default function IndexPage() {
         <meta charSet="utf-8" />
         <title>js pescasio | portfolio</title>
         <meta name="description" content="J.S. Pescasio (also known as claynaut) is an artist and developer based in Southern California." />
-        <link rel="apple-touch-icon" sizes="180x180" href="../images/apple-touch-icon.png" />
-        <link rel="icon" href="../images/favicon.ico" />
-        <link rel="icon" type="image/png" sizes="32x32" href="../images/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="../images/favicon-16x16.png" />
-        <link rel="manifest" href="../images/site.webmanifest" />
-        <link rel="mask-icon" href="../images/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
       </Helmet>
       <Home />
     </main>
