8fb093ae6f6864b0bc528b70d62a0fac80170fc2
claynaut <jpesc002@ucr.edu>
2021-02-05 11:47:09 -0800

    Update favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,11 +10,11 @@ export default function IndexPage() {
         <meta charSet="utf-8" />
         <title>js pescasio | portfolio</title>
         <meta name="description" content="J.S. Pescasio (also known as claynaut) is an artist and developer based in Southern California." />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="apple-touch-icon" sizes="180x180" href="../images/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="../images/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="../images/favicon-16x16.png" />
+        <link rel="manifest" href="../images/site.webmanifest" />
+        <link rel="mask-icon" href="../images/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
       </Helmet>
