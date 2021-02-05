2dd26e61608a4473707c82f76baaf212f7b23322
claynaut <jpesc002@ucr.edu>
2021-02-05 11:53:15 -0800

    Update favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,7 @@ export default function IndexPage() {
         <title>js pescasio | portfolio</title>
         <meta name="description" content="J.S. Pescasio (also known as claynaut) is an artist and developer based in Southern California." />
         <link rel="apple-touch-icon" sizes="180x180" href="../images/apple-touch-icon.png" />
+        <link rel="icon" href="../images/favicon.ico" />
         <link rel="icon" type="image/png" sizes="32x32" href="../images/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="../images/favicon-16x16.png" />
         <link rel="manifest" href="../images/site.webmanifest" />
